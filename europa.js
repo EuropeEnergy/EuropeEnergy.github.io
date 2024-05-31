dd701cc77e92d396d7b132bc44ca42137df8c331
NiklasSchw <schwardnik@yahoo.de>
2024-05-31 12:07:55 +0200

    Erstellung der Europakarte

---
+++ europa.js
@@ -0,0 +1,23 @@
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let mapeu = L.map("mapeu", {
+    fullscreenControl: true
+}).setView([ibk.lat, ibk.lng], 5);
+
+// Hintergrundlayer
+let layerControl = L.control.layers({
+    "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery").addTo(mapeu)
+}).addTo(mapeu);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(mapeu);
+
