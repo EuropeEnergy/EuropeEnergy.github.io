0a65bf09aea028e4dfdc22f33b667fe5a018b877
Danielwebmap <Daniel.Schoenung@student.uibk.ac.at>
2024-05-31 20:28:24 +0200

    GeoJson in Javascript integriert und anschließend ein GeoJSON Objekt erstellt, Funktionsaufruf showGeoJsonEU

--- europa.js
+++ europa.js
@@ -12,8 +12,8 @@ let mapeu = L.map("mapeu", {
 // Hintergrundlayer
 let layerControl = L.control.layers({
     "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
-    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery").addTo(mapeu)
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap").addTo(mapeu),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
 }).addTo(mapeu);
 
 // Maßstab
@@ -21,3 +21,21 @@ L.control.scale({
     imperial: false,
 }).addTo(mapeu);
 
+
+//DATENIMPORT GEOJSON EUROPA-DATEN
+
+async function showGeojsonEU(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+
+
+//ERSTELLUNG LEAFLET GEOJSON OBJEKT
+
+    L.geoJSON(geojson, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map)
+
+};
+
+showGeojsonEU("\Daten_europa.json");  //Funktionsaufruf 
